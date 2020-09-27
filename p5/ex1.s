58fd6f291fbc3fab56359d471902133997bb7e31
Paul Vazquez <pev@live.com.mx>
2020-09-26 19:39:09 -0700

    MIPS simulation

--- p5/ex1.s
+++ p5/ex1.s
@@ -8,12 +8,14 @@
 
 
 start: jal wait # Wait for button click
+nop
 jal wait2
 nop
 lui s0, 0xbf90 #Load switch port address
 lb s1, 0x0(s0) #read first number from switches
 nop
 jal wait #Wait for button click
+nop
 jal wait2
 nop
 lb s2, 0x0(s0) #Read second number from switches
@@ -25,17 +27,26 @@ nop
 ###Add code for wait subroutine here! ###
 
 
-wait: 	lui s5,0xbfa0
+wait: 	
+
+lui s5,0xbfa0
 		lw s4, 0(s5)
-		andi s4, 0x01
-		beq s4, s0, wait
+		andi s4, s4, 0x00000001
+		beq s4, r0, wait
+		nop
 		jr ra
+		nop
 		
-wait2:	lui s5,0xbfa0
+wait2:	
+
+lui s5,0xbfa0
+		and s4,r0,s4
 		lw s4, 0(s5)
-		andi s4, 0x01	
-		bne s4, s0, wait2
+		andi s4,s4, 0x00000001	
+		bne s4, r0, wait2
+		nop
 		jr ra
+		nop
 		
 		
 		
