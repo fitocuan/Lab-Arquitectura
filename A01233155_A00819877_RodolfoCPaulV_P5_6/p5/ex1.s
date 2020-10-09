d1598109e3ad5c5a8fe48c1fc14e021a90492446
Rodolfo Cuan Urquizo <rc19955@essex.ac.uk>
2020-10-09 15:58:43 -0500

    p7

---
+++ A01233155_A00819877_RodolfoCPaulV_P5_6/p5/ex1.s
@@ -0,0 +1,58 @@
+#include <iregdef.h>
+.set noreorder
+.text
+.globl start
+.ent start
+
+
+
+
+start: jal wait # Wait for button click
+nop
+jal wait2
+nop
+lui s0, 0xbf90 #Load switch port address
+lb s1, 0x0(s0) #read first number from switches
+nop
+jal wait #Wait for button click
+nop
+jal wait2
+nop
+lb s2, 0x0(s0) #Read second number from switches
+nop
+addu s3, s1, s2 #Perform an arithmetic operation
+sb s3, 0x0(s0) #Wirte the result to LEDs
+b start #Repeat all over again
+nop
+###Add code for wait subroutine here! ###
+
+
+wait: 	
+
+lui s5,0xbfa0
+		lb s4, 0(s5)
+		nop
+		andi s4, s4, 0x00000001
+		nop
+		beq s4, r0, wait
+		nop
+		jr ra
+		nop
+		
+wait2:	
+
+lui s5,0xbfa0
+		lb s4, 0(s5)
+		nop
+		andi s4,s4, 0x00000001	
+		nop
+		bne s4, r0, wait2
+		nop
+		jr ra
+		nop
+		
+		
+		
+
+
+.end start
